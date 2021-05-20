9a3adcdd728f9eb2d0bff7b8e55999d769bb246d
jumaris88 <40606311+jumaris88@users.noreply.github.com>
2021-05-20 23:14:15 +0800

    4

---
+++ jumaris/ares/data/key.php
@@ -0,0 +1,30 @@
+<?php
+$key = array(
+	"al-qur'an","al-quran","al qur'an",
+    "al quran","alquran","alqur'an",
+    "qur'an","quran","qs",
+    
+	"bukhari","muslim","nasai",
+	"darimi","malik","ahmad",
+	"tirmidzi",
+	"abu_daud","ibnu_majah",
+	"abu-daud","ibnu-majah",
+	"abu daud","ibnu majah",
+	
+	"bug","listbug","addfix","delfix",
+	"addhapuskata","delhapuskata",
+	"xreplace",
+	"random hadits",
+	"random qur'an",
+	"random quran",
+	"random al-qur'an",
+	"random al-quran",
+	"random al qur'an",
+	"random al quran",
+	"random alqur'an",
+	"random alquran",
+	"random qs",
+	
+	"terjemah ke"
+);
+?>
---
+++ jumaris/ares/data/perintah.php
@@ -0,0 +1,13 @@
+<?php
+$perintah = array(
+    "robotadd",
+    "robotdel",
+    "addfix",
+    "listrobot",
+    "listfix",
+    "bukhari","muslim","ahmad","malik",
+    "darimi","nasai","tirmidzi",
+    "abu_daud","ibnu_majah","quran","qs"
+    );
+
+?>
