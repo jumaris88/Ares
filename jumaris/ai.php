1bf4a79439ab2fcff60929e18d675d11b2db2291
jumaris88 <40606311+jumaris88@users.noreply.github.com>
2021-05-21 10:10:51 +0800

    update ai.php

--- jumaris/ai.php
+++ jumaris/ai.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require "../vendor/autoload.php";
+require "vendor/autoload.php";
 
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
